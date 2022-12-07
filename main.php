d1892aacb53c157a2649737b14e0e8a5c536d09b
Ridopandi123 <ridosinaga037@gmail.com>
2022-12-07 13:03:58 +0700

    fix an ui error

--- main.php
+++ main.php
@@ -29,7 +29,7 @@
 		<div id="header">
 			<?php include('alert.php'); ?>
 			<div>
-				<img class="img-fluid m-2 w-100" src="images/logo.png"/>
+				<!-- <img class="img-fluid m-2 w-100" src="images/logo.png"/> -->
 			</div>
 			<div class="text-white mt-0 ml-5">
 				<table class="table-responsive-sm">
@@ -54,6 +54,32 @@
 			<div class="header_price border p-0">
 				<h5>Grand Total</h5>
 				<p class="pb-0 mr-2" style="float: right; font-size: 40px;" id="totalValue">Rp 0.00</p>
+			</div>
+		</div>
+		<div id="sidebar">
+			<div class="mt-1 ">
+			<div class="input-group"><div class="input-group-prepend"><span class="input-group-text" id="basic-addon1"><i class="fas fa-search"></i></span></div>
+				   <input class="form-control" type="text" placeholder="Product Search" aria-label="Search" id="search" name="search" onkeyup="loadproducts();"/>
+			   </div></div>
+			<div id="product_area" class="table-responsive-sm mt-2 table-wrapper-scroll-y my-custom-scrollbar" >
+				<table class="w-100 table-striped font-weight-bold" style="cursor: pointer;" id="table1">
+					<thead>
+						<tr claclass='text-center'><b>
+							<td>Barcode</td>
+							<td>Product Name</td>
+							<td>Price</td>
+							<td>Unit</td>
+							<td>Stocks</td>
+							<td>Location</td>
+						</tr></b>
+						<tbody id="products">
+							
+						</tbody>
+					</thead>
+				</table>
+			</div>
+			<div class="w-100 mt-2" id="enter_area">
+				<button id="buttons" type="button" class="cancel btn btn-secondary border"><i class="fas fa-ban"></i> Cancel</button>
 			</div>
 		</div>
 		<div id="content" class="mr-2">
@@ -88,33 +114,8 @@
 				</div>
 			</div>
 		</div>
-		<div id="sidebar">
-			<div class="mt-1 ">
-			<div class="input-group"><div class="input-group-prepend"><span class="input-group-text" id="basic-addon1"><i class="fas fa-search"></i></span></div>
-   				<input class="form-control" type="text" placeholder="Product Search" aria-label="Search" id="search" name="search" onkeyup="loadproducts();"/>
-   			</div></div>
-			<div id="product_area" class="table-responsive-sm mt-2 table-wrapper-scroll-y my-custom-scrollbar" >
-				<table class="w-100 table-striped font-weight-bold" style="cursor: pointer;" id="table1">
-					<thead>
-						<tr claclass='text-center'><b>
-							<td>Barcode</td>
-							<td>Product Name</td>
-							<td>Price</td>
-							<td>Unit</td>
-							<td>Stocks</td>
-							<td>Location</td>
-						</tr></b>
-						<tbody id="products">
-							
-						</tbody>
-					</thead>
-				</table>
-			</div>
-			<div class="w-100 mt-2" id="enter_area">
-				<button id="buttons" type="button" class="cancel btn btn-secondary border"><i class="fas fa-ban"></i> Cancel</button>
-			</div>
-		</div>
-		<div id="footer" class="w-75">
+		
+		<div id="footer1" class="w-100">
 			<button id="buttons" onclick="window.location.href='user/user.php'" class="btn btn-secondary border mr-2 ml-2"><i class="fas fa-users"></i> User</button>
 			<button id="buttons" onclick="window.location.href='products/products.php'" class="btn btn-secondary border mr-2"><i class="fas fa-box-open"></i> Product</button>
 			<button id="buttons" onclick="window.location.href='supplier/supplier.php'" class="btn btn-secondary border mr-2"><i class="fas fa-user-tie"></i> Supplier</button>
@@ -123,8 +124,12 @@
 			<button id="buttons" onclick="window.location.href='cashflow/cashflow.php'" class="btn btn-secondary border mr-2"><i class="fas fa-money-bill-wave"></i> Cash-Flow</button>
 			<button id="buttons" onclick="window.location.href='sales/sales.php'" class="btn btn-secondary border mr-2"><i class="fas fa-shopping-cart"></i> Sales</button>
 			<button id="buttons" onclick="window.location.href='delivery/delivery.php'" class="btn btn-secondary border mr-2"><i class="fas fa-truck"></i> Deliveries</button>
+			<button id="buttons" onclick="window.location.href='delivery/delivery.php'" class="btn btn-secondary border mr-2"><i class="fas fa-truck"></i> Deliveries</button>
+			<button id="buttons" onclick="window.location.href=''" class="btn btn-secondary border mr-2"><i class=""></i> Report</button>
+			<button id="buttons" onclick="window.location.href=''" class="btn btn-secondary border mr-2"><i class=""></i> Report</button>
 			<button id="buttons" name="logout" type="button" onclick="out();" class="logout btn btn-danger border mr-2"><i class="fas fa-sign-out-alt"></i> Logout</button> 
 		</div>
+		
 	</div>
 	<?php include('add.php');?>
 	<?php include('templates/js_popper.php');?>
