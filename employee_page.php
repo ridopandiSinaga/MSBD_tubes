5845c8b38b55d419e361d58624bcb4bc4623a8a1
Ridopandi123 <ridosinaga037@gmail.com>
2022-12-21 19:09:00 +0700

    fix linked in sale to add sale

--- employee_page.php
+++ employee_page.php
@@ -21,7 +21,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>POS.PHP</title>
+	<title>Toko Kita</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="icon" type="image/png" sizes="180x180" href="images/icon.png">
--- loadproducts.php
+++ loadproducts.php
@@ -9,7 +9,7 @@
 		if(mysqli_num_rows($query)>0){
 			while($row = mysqli_fetch_array($query)){
 				echo "<tr class='js-add' data-barcode=".$row['product_no']." data-product=".$row['product_name']." data-price=".$row['sell_price']." data-unt=".$row['unit']."><td>".$row['product_no']."</td><td>".$row['product_name']."</td>";
-				echo "<td>₱".$row['sell_price']."</td>";
+				echo "<td>Rp.".$row['sell_price']."</td>";
 				echo "<td>".$row['unit']."</td>";
 				echo "<td>".$row['quantity']."</td>";
 				echo "<td>".$row['location']."</td>";
